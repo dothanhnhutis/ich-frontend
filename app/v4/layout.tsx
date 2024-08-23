1cbf71c8dfe8586bbec7d5f2f178b71dc60105f8
dothanhnhutis <dothanhnhutis@gmail.com>
2024-08-24 01:05:54 +0700

    update v4

--- app/v4/layout.tsx
+++ app/v4/layout.tsx
@@ -28,7 +28,7 @@ const V2Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="relative sm:flex">
       <div className="flex-shrink-0 sticky top-0 left-0 right-0 z-50 sm:top-0 sm:left-0 sm:bottom-0 backdrop-blur bg-background/60 sm:w-[200px] sm:h-screen flex items-center justify-between sm:block">
         <Link href="/" prefetch={false} className="flex items-center p-2">
-          <div className={cn("flex items-center size-10 sm:size-12")}>
+          <div className={cn("flex items-center size-12")}>
             <AspectRatio
               ratio={1 / 1}
               className="flex items-center justify-center"
@@ -44,14 +44,12 @@ const V2Layout = ({ children }: { children: React.ReactNode }) => {
             </AspectRatio>
           </div>
         </Link>
-
-        <div className="flex items-center gap-2 px-2">
-          <div className="flex items-center p-2 sm:hidden rounded-full hover:bg-primary/20 hover:text-primary">
-            <BellIcon className="block flex-shrink-0 size-6 sm:mr-2" />
-            <span className="text-sm hidden sm:inline">Settings</span>
+        <div className="flex items-center gap-2 px-2 sm:hidden">
+          <div className="flex items-center p-2 rounded-full hover:bg-primary/20 hover:text-primary">
+            <BellIcon className="block flex-shrink-0 size-6" />
           </div>
 
-          <div className="p-1 bg-accent rounded-full sm:hidden">
+          <div className="p-1 bg-accent rounded-full">
             <Avatar className="size-8">
               <AvatarImage
                 referrerPolicy="no-referrer"
@@ -63,13 +61,43 @@ const V2Layout = ({ children }: { children: React.ReactNode }) => {
             </Avatar>
           </div>
 
-          <div className="flex items-center p-2 sm:hidden rounded-full hover:bg-primary/20 hover:text-primary">
-            <MenuIcon className="block flex-shrink-0 size-6 sm:mr-2" />
-            <span className="text-sm hidden sm:inline">Settings</span>
+          <div className="flex items-center p-2 rounded-full hover:bg-primary/20 hover:text-primary">
+            <MenuIcon className="block flex-shrink-0 size-6" />
           </div>
         </div>
+        <div className="relative hidden sm:block sm:mt-5 sm:px-2 sm:max-h-full sm:overflow-h-scroll">
+          <Link
+            href="/"
+            className="flex items-center p-2 rounded-lg mb-1 last:mb-0 hover:bg-primary/20 hover:text-primary"
+          >
+            <UserIcon className="block flex-shrink-0 size-6 sm:mr-2" />
+            <span className="text-sm hidden sm:inline">Users</span>
+          </Link>
+          <Link
+            href="/"
+            className="flex items-center p-2 rounded-lg mb-1 last:mb-0 bg-primary/20 text-primary"
+          >
+            <UserIcon className="block flex-shrink-0 size-6 sm:mr-2" />
+            <span className="text-sm hidden sm:inline">Posts</span>
+          </Link>
+          <Link
+            href="/"
+            className="flex items-center p-2 rounded-lg mb-1 last:mb-0 hover:bg-primary/20 hover:text-primary"
+          >
+            <UserIcon className="block flex-shrink-0 size-6 sm:mr-2" />
+            <span className="text-sm hidden sm:inline">Products</span>
+          </Link>
+
+          <Link
+            href="/"
+            className="flex items-center p-2 rounded-lg mb-1 last:mb-0 hover:bg-primary/20 hover:text-primary"
+          >
+            <SettingsIcon className="block flex-shrink-0 size-6 sm:mr-2" />
+            <span className="text-sm hidden sm:inline">Settings</span>
+          </Link>
+        </div>
       </div>
-      <div className="w-full">
+      <div>
         <div className="sticky top-0 left-0 right-0 z-50 backdrop-blur bg-background/60 hidden p-2 sm:flex sm:justify-end sm:items-center sm:gap-2">
           <div className="p-2 rounded-full hover:bg-primary/20 hover:text-primary">
             <BellIcon className=" flex-shrink-0 size-6" />
--- app/v4/users/page.tsx
+++ app/v4/users/page.tsx
@@ -11,90 +11,117 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
 import AvatarDefault from "@/images/avatars/user-1.jpg";
-import { ChevronDownIcon, LogOutIcon, MailIcon } from "lucide-react";
+import {
+  ChevronDownIcon,
+  EllipsisVerticalIcon,
+  ListFilterIcon,
+  LogOutIcon,
+  MailIcon,
+  PlusIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { Label } from "@/components/ui/label";
 import { DarkMode } from "@/components/switch-theme";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const UserManagementPage = () => {
   return (
-    <div className="bg-background h-full border rounded-xl p-4">
-      <div className="flex justify-between">
-        <div>
-          <h3 className="font-bold text-2xl">User management</h3>
-          <p className="text-sm font-normal leading-snug text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
+    <div className="bg-background rounded-xl p-4">
+      <h3 className="font-bold text-2xl">User management</h3>
+      <p className="text-sm font-normal leading-snug text-muted-foreground">
+        Manage your user and their account permission here.
+      </p>
+      <div className="flex flex-col mt-4 gap-4 h-[2000px]">
+        <div className="flex gap-2 items-center overflow-x-scroll">
+          <Button variant="secondary">Active</Button>
+          <Button variant="ghost">Suspended</Button>
+          <Button variant="ghost">Disabled</Button>
         </div>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="outline-none" asChild>
-            <div className="flex items-center gap-2 bg-background p-2 cursor-pointer shadow rounded-full">
-              <Avatar className="size-8">
-                <AvatarImage
-                  referrerPolicy="no-referrer"
-                  src={AvatarDefault.src}
-                />
-                <AvatarFallback className="bg-transparent">
-                  <Skeleton className="size-8 rounded-full" />
-                </AvatarFallback>
-              </Avatar>
-              <div className="hidden sm:block max-w-[120px]">
-                <p className="font-bold truncate text-sm ">Thành Nhựt</p>
-                <p className="text-muted-foreground text-xs">Admin</p>
-              </div>
-              <div className="hidden sm:block p-1 rounded-full bg-muted">
-                <ChevronDownIcon className="flex flex-shrink-0 size-2" />
-              </div>
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-[360px]">
-            <DropdownMenuLabel className="flex items-center gap-3">
-              <Avatar className="size-24">
-                <AvatarImage
-                  referrerPolicy="no-referrer"
-                  src={AvatarDefault.src}
-                />
-                <AvatarFallback className="bg-transparent">
-                  <Skeleton className="size-24 rounded-full" />
-                </AvatarFallback>
-              </Avatar>
-
-              <div className="w-full overflow-hidden">
-                <p className="font-medium text-lg">Thành Nhựt</p>
-                <p className="text-muted-foreground font-normal">Admin</p>
-                <div className="flex items-center space-x-2 text-muted-foreground w-full">
-                  <MailIcon className="flex flex-shrink-0 size-4" />
-                  <p className="text-sm truncate">gaconght@gmail.com</p>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-
-            <DropdownMenuItem asChild>
-              <Link
-                href="/account/settings"
-                className="cursor-pointer h-[34px]"
-              >
-                <span className="font-medium">Settings</span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Label htmlFor="dark-mode" className="flex justify-between">
-                <span>Dark mode</span>
-                <DarkMode id="dark-mode" />
-              </Label>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <LogOutIcon className="mr-2 h-4 w-4" />
-              <span className="font-medium">Log out</span>
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex justify-between">
+          <h4 className="font-bold text-lg">All users (100)</h4>
+          <div className="flex items-center gap-2">
+            <Button className="px-2 gap-1" variant="outline" size="sm">
+              <ListFilterIcon className="size-4 flex-shrink-0" />
+              Filters
+            </Button>
+            <Button className="px-2 gap-1" size="sm">
+              <PlusIcon className="size-6 flex-shrink-0" />
+              Add user
+            </Button>
+          </div>
+        </div>
+        <ScrollArea className="border rounded-lg max-w-full">
+          <Table className="table-fixed max-w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="relative w-[400px]">User name</TableHead>
+                <TableHead className="relative w-[200px]">Role</TableHead>
+                <TableHead className="relative w-[200px]">Verified</TableHead>
+                <TableHead className="relative w-[200px]">Date added</TableHead>
+                <TableHead className="relative w-[50px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="flex items-center gap-2">
+                  <Avatar className="size-10">
+                    <AvatarImage
+                      referrerPolicy="no-referrer"
+                      src={AvatarDefault.src}
+                    />
+                    <AvatarFallback className="bg-transparent">
+                      <Skeleton className="size-8 rounded-full" />
+                    </AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="font-bold text-base">Thanh Nhut</p>
+                    <p className="text-xs text-muted-foreground">
+                      gaconght@gmail.com
+                    </p>
+                  </div>
+                </TableCell>
+                <TableCell>Admin</TableCell>
+                <TableCell>True</TableCell>
+                <TableCell>ds</TableCell>
+                <TableCell>
+                  <EllipsisVerticalIcon className="size-4 flex-shrink-0" />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="flex items-center gap-2">
+                  <Avatar className="size-10">
+                    <AvatarImage
+                      referrerPolicy="no-referrer"
+                      src={AvatarDefault.src}
+                    />
+                    <AvatarFallback className="bg-transparent">
+                      <Skeleton className="size-8 rounded-full" />
+                    </AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="font-bold text-base">Thanh Nhut</p>
+                    <p className="text-xs text-muted-foreground">
+                      gaconght@gmail.com
+                    </p>
+                  </div>
+                </TableCell>
+                <TableCell>Admin</TableCell>
+                <TableCell>ds</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <ScrollBar orientation="horizontal" asChild />
+        </ScrollArea>
       </div>
-
-      <div className="flex flex-col mt-4 gap-4"></div>
     </div>
   );
 };
